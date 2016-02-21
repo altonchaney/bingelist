e900a790b9e4b4d175aa18251009deeb67aa19bf
Alton Chaney <alton.chaney@gmail.com>
2016-02-21 14:30:32 -0600

    new test list; try out some counters for episode types (not going so well yet)

--- js/controllers.js
+++ js/controllers.js
@@ -10,19 +10,34 @@ angular.module('bingelist-controllers', [])
 })
 
 .controller('tvshowCtrl', function($scope, $http, $routeParams){
+  
   //var showId = $routeParams.showId; //testing one show
-  var showId = 563;
+  var showId = 158;
+  
   $http.get('http://api.tvmaze.com/shows/' + showId + '?embed=previousepisode')
     .then(function(tvshow){
       $scope.tvshow = tvshow.data;
     });
+  
   $http.get('http://api.tvmaze.com/shows/' + showId + '/episodes')
     .then(function(episodes){
       $scope.episodes = episodes.data;
       $scope.maxSeasons = Math.max.apply(Math,$scope.episodes.map(function(episode){return episode.season;}));
     });
+  
   $http.get('../list/' + showId + '.json')
     .then(function(bingelist){
       $scope.bingelist = bingelist.data;
+      var countObj = { };
+			for (var i = 0, j = bingelist.length; i < j; i++) {
+				 countObj[bingelist[i]] = (countObj[bingelist[i]] || 0) + 1;
+			};
+			
+			console.log(countObj["required"]);
+			$scope.requiredCount = countObj["required"];
+			$scope.maybeCount = countObj["maybe"];
+			$scope.fillerCount = countObj["filler"];
     });
 });
+
+			
